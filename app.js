330c56580c3602a0b93fbdc43b36ac84ddc992dd
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-08-08 21:06:29 +0530

    user models done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect;
-//const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,20 +16,21 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { getMaxListeners } = require('process');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(async (req, res, next) => {
-//   try {
-//     const user = await User.findById('64d0d26b82be7a4440ad02f8');
-//     req.user = new User(user.name, user.email, user.cart|| { items: [] }, user._id);
-//     next();
-//   } catch (err) {
-//     console.error(err);
-//     next(); 
-//   }
-// });
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('64d25d0c18696c1e07280e9b');
+    req.user =user;
+    next();
+  } catch (err) {
+    console.error(err);
+    next(); 
+  }
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +39,20 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://Nirmalya:ZVuBsk9zzZKzjnmU@cluster.gounyfp.mongodb.net/shop?retryWrites=true&w=majority')
 .then(result=>{
+  User.findOne().then(user=>{
+    if(!user)
+    {
+      const user = new User({
+        name:'Nirmalya Sengupta',
+        email: 'nir@gmail.com',
+        cart:{
+          items:[]
+        }
+      })
+      user.save();
+    }
+  })
+  
   app.listen(3000);
 })
 .catch(err=>{
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({title:title, price:price, description:description, imageUrl:imageUrl})
+  const product = new Product({title:title, price:price, description:description, imageUrl:imageUrl,userId:req.user})
   product
     .save()
     .then(result => {
@@ -70,6 +70,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+  
     .then(products => {
       res.render('admin/products', {
         prods: products,
